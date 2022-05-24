b1e09019a0c02880c780601fb7faa73abb59febb
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-24 20:26:19 +0530

    add

---
+++ src/Components/Add.js
@@ -0,0 +1,68 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+const Add = () => {
+    var [name,setname]=useState("")
+    var [date,setdate]=useState("")
+    var [venue,setvenue]=useState("")
+    var [organiser,setorganiser]=useState("")
+    var [contact,setcontact]=useState("")
+    const subdata=()=>{
+        const data={"name":name,"date":date,"venue":venue,"organiser":organiser,"contact":contact}
+        console.log(data)
+    axios.post("https://mylinkurcodesapp.herokuapp.com/addcourse",data).then(
+        (response)=>
+        {
+            console.log(response.data)
+            if(response.data.status=="success")
+            {
+                alert("successfully inserted")
+            }
+            else
+            {
+                alert("failed ")
+            }
+        }
+        )}
+  return (
+    <div>
+
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <dv className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="for">NAME</label>
+                    <input onChange={(c)=>{setname(c.target.value)}} type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="for">DATE</label>
+                    <input onChange={(c)=>{setdate(c.target.value)}} type="date" name="" id="" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="for">VENUE</label>
+                    <input onChange={(c)=>{setvenue(c.target.value)}} type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="for">ORGANISER</label>
+                    <input onChange={(c)=>{setorganiser(c.target.value)}} type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="for">CONTACT NUM</label>
+                    <input onChange={(c)=>{setcontact(c.target.value)}} type="text" className="form-control"/>
+                    
+
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button  onClick={subdata} className="btn btn-success">SUBMIT</button>
+                </div>
+                
+            </dv>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default Add
